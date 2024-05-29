ab21e69d1c06743debd16cfe233912cba3f89984
Thinh Nguyen <thinhntr@gmail.com>
2024-05-29 23:10:03 +0700

    Revert "fix backend CORS"
    This reverts commit 46aef8b037cfd43cc112497232f8112ed23ebb91.

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 class Item(BaseModel):
